56d42f9e4afd2db445cb610963ec9ae624f04194
Brian Cassidy <bricas@cpan.org>
2011-03-17 09:57:58 -0300

    slight modification to multiple cols. case insenstive ordering of dists.

--- lib/CPAN/Changes/Web/Schema/Result/Release.pm
+++ lib/CPAN/Changes/Web/Schema/Result/Release.pm
@@ -62,7 +62,8 @@ __PACKAGE__->add_columns(
     },
 );
 __PACKAGE__->set_primary_key( 'id' );
-__PACKAGE__->resultset_attributes( { order_by => [ 'distribution' ] } );
+# "COLLATE NOCASE" is an SQLite-ism
+__PACKAGE__->resultset_attributes( { order_by => [ 'distribution COLLATE NOCASE' ] } );
 __PACKAGE__->add_unique_constraint(
     release_key => [ qw( distribution author version ) ], );
 
