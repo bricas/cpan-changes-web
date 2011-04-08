bd53c11f9c3d0f37d8fa3b76355ea22f5e62e1d4
Brian Cassidy <bricas@cpan.org>
2011-04-08 16:12:41 -0300

    only index authors with releases in current scan

--- bin/authors.pl
+++ bin/authors.pl
@@ -17,11 +17,12 @@ GetOptions(
 );
 
 my $schema  = CPAN::Changes::Web::schema( 'db' );
+my @authors = $schema->resultset( 'Scan' )->first->releases( {}, { group_by => 'author', order_by => 'author' } )->get_column( 'author' )->all;
 my $rs      = $schema->resultset( 'Author' );
 my $p       = Parse::CPAN::Authors->new( "$minicpan/authors/01mailrc.txt.gz" );
 my $counter = 0;
 
-for my $author ( $p->authors ) {
+for my $author ( map { $p->author( $_ ) } @authors ) {
     printf "\r[%04d] %-50s", ++$counter, $author->pauseid;
 
     $rs->update_or_create( {
